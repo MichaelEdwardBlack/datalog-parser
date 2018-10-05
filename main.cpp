5636edff1f8d2612fac1c1bee244ad1fd004248b
michaelblack117 <michaelblack117@gmail.com>
2018-10-05 01:02:44 -0600

    Copy of lexical analyzer (lab1)

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "lexer.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	//OPEN FILES
+	ifstream in(argv[1]);
+	//ofstream out(argv[2]);
+
+	Lexer lex;
+	lex.scan(in);
+
+	cout << lex.printTokens() << "Total Tokens = " << lex.getNumTokens();
+
+	in.close();
+	//out.close();
+
+	return 0;
+}
